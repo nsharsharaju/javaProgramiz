14425953176b249f39e700fe65c4777cec605e91
Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>
2021-01-26 07:24:51 +0530

    Add more Java Programs Signed-off-by: Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>

---
+++ AddTwoIntegers.java
@@ -0,0 +1,11 @@
+public class AddTwoIntegers {
+    public static void main(String arngs[]) {
+        int first = 10;
+        int second = 20;
+
+        System.out.println("Enter two numbers: " + first + " " + second);
+        int sum = first + second;
+
+        System.out.println("The sum is: " + sum);
+    }
+}
